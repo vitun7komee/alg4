ce370aa8d17441ceebed14e273c6ff43af2afba0
vitun7komee <118392333+vitun7komee@users.noreply.github.com>
2022-12-19 00:15:46 +0300

    Add files via upload

---
+++ alg4.cpp
@@ -0,0 +1,37 @@
+﻿#define MAX 2502
+#include<iostream>
+#include<string>
+double min(double x, double y) {
+    
+    if (x > y) {
+        return y;
+    }
+    else {
+        return x;
+    }
+}
+int main() {
+
+	std::string boards;
+	double c[MAX];
+
+    while (std::cin >> boards)
+    {
+        boards = " " + boards;
+        c[0] = 0;
+        for (int i = 1; i < boards.length(); i++)
+        {
+
+            c[i] = 10000;
+            if (boards[i] == '.') {
+                c[i] = c[i - 1];
+            }
+            else {
+                for (int j = 0; j < i; j++)
+                    c[i] = min(c[i], c[j] + sqrt(1.0 * i - j));
+            }
+
+        }
+        printf("%.4lf\n", c[boards.length() - 1]);
+    }
+}
